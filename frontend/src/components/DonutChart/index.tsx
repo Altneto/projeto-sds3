86e2b7626ebde62bf7b80a15d5c14352251d8941
Altneto <neto1171995@gmail.com>
2021-09-14 00:31:12 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,40 +1,39 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import Chart from 'react-apexcharts'
-import { SaleSum } from 'types/sale';
-import { BASE_URL } from 'utils/requests';
+import { SaleSum } from 'types/sale'
+import { BASE_URL } from 'utils/requests'
 
 type ChartData = {
-    labels: string[];
-    series: number[];
+    labels: string[]
+    series: number[]
 }
-
-const options = {
-    legend: {
-        show: true
-    }
-}
-
-/*const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-}*/
 
 function DonutChart() {
-
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] })
 
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then(response => {
-                const data = response.data as SaleSum[];
-                const myLabels = data.map(x => x.sellerName);
-                const mySeries = data.map(x => x.sum);
+                const data = response.data as SaleSum[]
+                const myLabels = data.map(x => x.sellerName)
+                const mySeries = data.map(x => x.sum)
 
-                setChartData({ labels: myLabels, series: mySeries });
-            });
-    }, []);
+                setChartData({ labels: myLabels, series: mySeries })
+            })
+    }, [])
 
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+    
+    /*const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    }*/
 
     return (
         <Chart
@@ -43,7 +42,7 @@ function DonutChart() {
             type="donut"
             height="240"
         />
-    );
+    )
 }
 
-export default DonutChart;
+export default DonutChart
